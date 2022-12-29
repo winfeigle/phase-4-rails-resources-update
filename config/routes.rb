1970d9f1219590c026adb394eb27a848e63d1266
Win Feigle <win.feigle@gmail.com>
2022-12-29 14:41:08 -0700

    Complete assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :update]
+
+  # Custom route to handle likes from the backend
+  patch "/birds/:id/like", to: "birds#increment_likes"
 end
